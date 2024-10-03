2b7a100b3af82fcf25c4b30739a3dd8822e7633a
GIL ALFREDO CASIMIRO RAMI <gil.casimiro@elektra.com.mx>
2024-10-03 14:57:53 -0600

    Initial Commit

---
+++ Liverpool_App/Liverpool_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liverpool_AppApp.swift
+//  Liverpool_App
+//
+//  Created by Gil casimiro on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Liverpool_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
